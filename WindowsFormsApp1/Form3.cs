c95c7a797d3f7f2e9d9ceb92ae38c8275c339cb1
진욱 나 <skwlsdnr94@naver.com>
2018-11-12 17:49:46 +0900

    판넬을 이용한 회원 검색기능 화면 구성

--- WindowsFormsApp1/Form3.cs
+++ WindowsFormsApp1/Form3.cs
@@ -20,27 +20,6 @@ namespace WindowsFormsApp1
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            //Label ID_label = new Label();
-            //ID_label.Text = "회원 ID / 이름";
-            //ID_label.Size = new Size(165, 30);
-            //ID_label.Location = new Point(30, 30);
-            //Controls.Add(ID_label);
-            //ID_label.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
-
-            //TextBox Content = new TextBox();
-            //Content.Size = new Size(200, 200);
-            //Content.Location = new Point(210, 30);
-            //Content.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
-            //Controls.Add(Content);
-
-            //Button Search_Btn = new Button();
-            //Search_Btn.Text = "검색";
-            //Search_Btn.Size = new Size(70, 40);
-            //Search_Btn.Location = new Point(410, 29);
-            //Controls.Add(Search_Btn);
-            //Search_Btn.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
-            //Search_Btn.BackColor = Color.Black;
-            //Search_Btn.ForeColor = Color.White;
             
         }
 
@@ -50,30 +29,58 @@ namespace WindowsFormsApp1
             Label ID_label = new Label();
             TextBox Content = new TextBox();
             Button Search_Btn = new Button();
+            Label Seat_Num = new Label();
+            Label Name = new Label();
+            Label userID = new Label();
+            ListView Search_View = new ListView();
 
             panel1.Location = new Point(10, 10);
-            panel1.Size = new Size(780, 430);
+            panel1.Size = new Size(590, 280);
             panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 
             ID_label.Text = "회원 ID / 이름";
             ID_label.Size = new Size(165, 30);
             ID_label.Location = new Point(30, 30);
             ID_label.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
+
             Content.Text = "";
-            Content.Size = new Size(200, 200);
+            Content.Size = new Size(260, 200);
             Content.Location = new Point(210, 30);
             Content.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
+
             Search_Btn.Text = "검색";
             Search_Btn.Size = new Size(70, 40);
-            Search_Btn.Location = new Point(410, 29);
+            Search_Btn.Location = new Point(470, 29);
             Search_Btn.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
             Search_Btn.BackColor = Color.Black;
             Search_Btn.ForeColor = Color.White;
+
+            Seat_Num.Text = "좌석 번호";
+            Seat_Num.Size = new Size(100, 30);
+            Seat_Num.Location = new Point(70, 80);
+            Seat_Num.Font = new Font(FontFamily.GenericSansSerif, 15.0F, FontStyle.Bold);
+
+            Name.Text = "이름";
+            Name.Size = new Size(100, 30);
+            Name.Location = new Point(250, 80);
+            Name.Font = new Font(FontFamily.GenericSansSerif, 15.0F, FontStyle.Bold);
+
+            userID.Text = "회원 ID";
+            userID.Size = new Size(100, 30);
+            userID.Location = new Point(410, 80);
+            userID.Font = new Font(FontFamily.GenericSansSerif, 15.0F, FontStyle.Bold);
+
+            Search_View.Size = new Size(550, 150);
+            Search_View.Location = new Point(20, 110);
 
             this.Controls.Add(panel1);
             panel1.Controls.Add(ID_label);
             panel1.Controls.Add(Content);
             panel1.Controls.Add(Search_Btn);
+            panel1.Controls.Add(Seat_Num);
+            panel1.Controls.Add(Name);
+            panel1.Controls.Add(userID);
+            panel1.Controls.Add(Search_View);
         }
     }
 }
