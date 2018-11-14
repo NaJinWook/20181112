f9742b14d0d1a586d55f82c6a8213265abcf8d08
진욱 나 <skwlsdnr94@naver.com>
2018-11-14 17:51:20 +0900

    2018-11-14 17:51 수정

--- WindowsFormsApp1/Search.cs
+++ WindowsFormsApp1/Search.cs
@@ -36,6 +36,7 @@ namespace WindowsFormsApp1
             Label ID_label = new Label();
             Content = new TextBox();
             Button Search_Btn = new Button();
+            Button Remove_Btn = new Button();
             ListView Search_View = new ListView();
 
             pan.Location = new Point(10, 10);
@@ -45,23 +46,31 @@ namespace WindowsFormsApp1
 
             ID_label.Text = "회원 ID / 이름";
             ID_label.Size = new Size(165, 30);
-            ID_label.Location = new Point(30, 30);
+            ID_label.Location = new Point(15, 30);
             ID_label.ForeColor = Color.White;
             ID_label.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
 
             Content.Name = "Content";
             Content.Text = "";
-            Content.Size = new Size(260, 200);
-            Content.Location = new Point(210, 30);
+            Content.Size = new Size(250, 200);
+            Content.Location = new Point(180, 30);
             Content.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
 
             Search_Btn.Text = "검색";
             Search_Btn.Size = new Size(70, 40);
-            Search_Btn.Location = new Point(470, 29);
+            Search_Btn.Location = new Point(430, 29);
             Search_Btn.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
             Search_Btn.BackColor = Color.DimGray;
             Search_Btn.ForeColor = Color.White;
             Search_Btn.Click += Search_Btn_click;
+
+            Remove_Btn.Text = "삭제";
+            Remove_Btn.Size = new Size(70, 40);
+            Remove_Btn.Location = new Point(500, 29);
+            Remove_Btn.Font = new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold);
+            Remove_Btn.BackColor = Color.DimGray;
+            Remove_Btn.ForeColor = Color.White;
+            Remove_Btn.Click += Remove_Btn_click;
 
 
             ColumnHeader columnHeader1 = new ColumnHeader();
@@ -101,12 +110,11 @@ namespace WindowsFormsApp1
             ListViewItem item4 = new ListViewItem();
             Search_View.Items.AddRange(new ListViewItem[] { item1, item2, item3, item4 });
 
-
-
             Controls.Add(pan);
             pan.Controls.Add(ID_label);
             pan.Controls.Add(Content);
             pan.Controls.Add(Search_Btn);
+            pan.Controls.Add(Remove_Btn);
             pan.Controls.Add(Search_View);
 
         }
@@ -126,11 +134,13 @@ namespace WindowsFormsApp1
                     item1.SubItems.Add("천호성");
                     item1.SubItems.Add("hosung1000");
                 }
-                else if(Content.Text == "")
-                {
-                    item1.SubItems.Clear();
-                }
             }
+        }
+        public void Remove_Btn_click(object o, EventArgs e)
+        {
+            item1.SubItems.Clear();
+            Content.Text = "";
+            Content.Focus();
         }
     }
 }
